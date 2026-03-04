2675331a0a43568ca00976595f5170d76d7db51b
Ronei Costa Soares <ronei.costa@aplicativo.net>
2026-03-04 14:08:10 -0300

    .

--- assets/js/App.js
+++ assets/js/App.js
@@ -9,7 +9,7 @@ class App {
         this.cifraPlayer = new CifraPlayer(this.elements, this.uiController, this.musicTheory, this.BASE_URL);
 
         this.versionConfig = {
-            version: '6.0.2',
+            version: '6.0.3',
             htmlMessage: `
                 <p>Melhorias</p>
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = '6.0.2';
+const version = '6.0.3';
 const CACHE_NAME = 'cifra-app-cache-' + version;
 
 const urlsToCache = [
