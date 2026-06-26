07c43f6b3a8d98d50529541cb546e276dcef4e7a
Ronei Costa Soares <ronei.costa@aplicativo.net>
2026-06-26 16:16:56 -0300

    version

--- assets/js/App.js
+++ assets/js/App.js
@@ -41,7 +41,7 @@ class App {
         };
 
         this.versionConfig = {
-            version: '6.1.1',
+            version: '6.1.2',
             htmlMessage: `
                 <p>Melhorias</p>
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = '6.1.1';
+const version = '6.1.2';
 const CACHE_NAME = 'cifra-app-cache-' + version;
 
 const urlsToCache = [
